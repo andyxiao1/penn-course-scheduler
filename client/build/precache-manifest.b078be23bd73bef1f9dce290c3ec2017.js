08ff025146b6fd4d5e6f673d62bdd30ccfc4f5f5
Andy Xiao <andyxiao@seas.upenn.edu>
2019-04-24 04:12:46 -0400

    Add build

---
+++ client/build/precache-manifest.b078be23bd73bef1f9dce290c3ec2017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cca8f8fd6dbb45097b8e",
+    "url": "/static/css/main.5f4f5ac0.chunk.css"
+  },
+  {
+    "revision": "cca8f8fd6dbb45097b8e",
+    "url": "/static/js/main.45168fb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6da61136dd4676c34b84",
+    "url": "/static/css/2.8d21b21d.chunk.css"
+  },
+  {
+    "revision": "6da61136dd4676c34b84",
+    "url": "/static/js/2.9dd112dd.chunk.js"
+  },
+  {
+    "revision": "580b5ec64f8d5f080ac6a7ddf53c689d",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 const PORT = process.env.NODE_ENV || 3001;
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('hi');
   app.use(express.static('client/build'));
 }
 
